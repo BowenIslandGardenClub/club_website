be9a636d4d06006ba04c7c168aed791ef4980865
Donald Heth <bahamadon@mac.com>
2015-09-12 14:59:17 -0700

    Major updates to site

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var Metalsmith = require('metalsmith');
 var layouts = require('metalsmith-layouts');
 var nunjucks = require('nunjucks');
 var handlebars = require('handlebars');
-var markdown = require('metalsmith-markdown');
 var contentful = require('contentful-metalsmith');
 var moment = require('moment-timezone');
 var ESAPI = require('node-esapi');
+var sortBy = require('sort-by');
+
+var marked = require('marked');
+marked.setOptions({
+  sanitize : true,
+  smartypants : true});
+  
 
 var logFilesMap = function(files, metalsmith, done) {
     Object.keys(files).forEach(function (file) {
@@ -39,7 +45,6 @@ console.log("starting Metalsmith now");
 Metalsmith(__dirname)
     .destination('./build')
 //    .use(plugin)
-    .use(markdown())
     .use(contentful({
       accessToken : '523ad29cf40ce3587d53e637fd52aa49bade02364e7d95f9f12de9a85fbf777a'}))
 //    .use(logFilesMap)
@@ -48,13 +53,16 @@ Metalsmith(__dirname)
       engine: 'nunjucks',
       directory: 'templates',
       moment: moment,
-      ESAPI: ESAPI}
+      sortBy: sortBy,
+      marked: marked,
+      ESAPI: ESAPI,
+      marked : marked}
     ))
     .build(function(err) {
         if (err) {
             console.log(err);
         }
         else {
-            console.log('Great work master');
+            console.log('Finished with Metalsmith');
         }
     });
